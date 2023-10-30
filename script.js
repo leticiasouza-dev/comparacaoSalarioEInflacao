3c854f92aaa8dec761277275754456ad3f5e3d37
leticiaSouzaDev <lec667121@gmail.com>
2023-10-30 16:07:34 -0300

    script da aplicação e suas dependencias

---
+++ script.js
@@ -0,0 +1,4 @@
+import input from 'readline-sync';
+
+// O objetivo da aplicação é listar o histórico do salário mínimo e da inflação (IPCA) entre os anos de 2010 e 2020. Além disso, o sistema deve ser capaz de calcular o crescimento percentual do salário mínimo ano a ano nesse período.
+
