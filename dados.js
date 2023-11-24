c8edb8dca23628e193979bd8bebfe1cccca69c18
leticiaSouzaDev <lec667121@gmail.com>
2023-11-24 11:46:47 -0300

    adicionando a função de escolha do usuário e a coleção de dados

---
+++ dados.js
@@ -0,0 +1,29 @@
+const salario_minimo = [
+    {Ano: 2010, Salario_minimo: 510},
+    {Ano: 2011, Salario_minimo: 545},
+    {Ano: 2012, Salario_minimo: 622},
+    {Ano: 2013, Salario_minimo: 678},
+    {Ano: 2014, Salario_minimo: 724},
+    {Ano: 2015, Salario_minimo: 788},
+    {Ano: 2016, Salario_minimo: 880},
+    {Ano: 2017, Salario_minimo: 937},
+    {Ano: 2018, Salario_minimo: 954},
+    {Ano: 2019, Salario_minimo: 998},
+    {Ano: 2020, Salario_minimo: 1045},
+]
+
+const inflaçãoIPCA = [
+    {Ano: 2010, inflacao_IPCA: 5.91},
+    {Ano: 2011, inflacao_IPCA: 6.50},
+    {Ano: 2012, inflacao_IPCA: 5.84},
+    {Ano: 2013, inflacao_IPCA: 5.91},
+    {Ano: 2014, inflacao_IPCA: 6.41},
+    {Ano: 2015, inflacao_IPCA: 10.67},
+    {Ano: 2016, inflacao_IPCA: 6.29},
+    {Ano: 2017, inflacao_IPCA: 2.95},
+    {Ano: 2018, inflacao_IPCA: 3.75},
+    {Ano: 2019, inflacao_IPCA: 4.31},
+    {Ano: 2020, inflacao_IPCA: 4.52}
+]
+
+export {salario_minimo, inflaçãoIPCA};
--- script.js
+++ script.js
@@ -1,4 +1,46 @@
 import input from 'readline-sync';
+import {salario_minimo, inflaçãoIPCA} from './dados.js'
 
 // O objetivo da aplicação é listar o histórico do salário mínimo e da inflação (IPCA) entre os anos de 2010 e 2020. Além disso, o sistema deve ser capaz de calcular o crescimento percentual do salário mínimo ano a ano nesse período.
 
+const escolhaUsuario = (escolha) => {
+
+    console.log("Escolha uma das alternativas: \n");
+    console.log("1 - Listar os salários minímos de 2010 á 2020 \n2 - Listar o índice IPCA de 2010 á 2020 \n3 - Comparação entre o percentual de aumento salarial e o IPCA")
+    
+    escolha = input.question("\nDigite o numero da sua escolha: ");
+
+    console.log(escolha);
+    if(escolha == 1){
+        for(let indice of salario_minimo){
+            console.log(indice);
+        }
+    } 
+    else if(escolha == 2){
+        for( let indice of inflaçãoIPCA){
+            console.log(indice);
+        }
+    } 
+    else{
+        
+    }
+}
+
+escolhaUsuario();
+
+
+
+// console.log(opcao);
+
+// switch(opcao){
+//     case 1:
+//         console.log("foiii");
+//         break
+
+//     case 2:
+//         console.log("foiiii222");
+//         break
+
+//     case 3: 
+//         console.log("foii33333");
+// }
